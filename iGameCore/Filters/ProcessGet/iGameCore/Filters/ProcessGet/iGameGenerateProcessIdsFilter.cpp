ed95506cd74bcb2067293d83a0a36bcd8f528d14
Tutis(图特斯） <152156470+BuzzyKey@users.noreply.github.com>
2026-08-21 22:10:42 +0800

    Create iGameGenerateProcessIdsFilter.cpp
    进程数组ID生成的.cpp代码文件

---
+++ iGameCore/Filters/ProcessGet/iGameCore/Filters/ProcessGet/iGameGenerateProcessIdsFilter.cpp
@@ -0,0 +1,46 @@
+#include "iGameGenerateProcessIdsFilter.h"
+
+IGAME_NAMESPACE_BEGIN
+
+GenerateProcessIdsFilter::GenerateProcessIdsFilter() {
+    SetNumberOfInputs(1);
+    SetNumberOfOutputs(1);
+}
+
+bool GenerateProcessIdsFilter::Execute() {
+    auto input = GetInput(0);
+    if (input == nullptr) return false;
+
+    auto mesh = DynamicCast<PointSet>(input);
+    if (mesh == nullptr) return false;
+
+    if (m_GeneratePointData) {
+        IntArray::Pointer ids = IntArray::New();
+        ids->SetName("process_ids");
+        IGsize pointNum = mesh->GetNumberOfPoints();
+        ids->Resize(pointNum);
+        for (IGsize i = 0; i < pointNum; ++i) {
+            ids->SetValue(i, m_ProcessId);
+        }
+        input->GetAttributeSet()->AddScalar(IG_POINT, ids);
+    }
+
+    if (m_GenerateCellData) {
+        auto unstructuredMesh = DynamicCast<UnstructuredMesh>(input);
+        if (unstructuredMesh != nullptr) {
+            IntArray::Pointer ids = IntArray::New();
+            ids->SetName("process_ids");
+            IGsize cellNum = unstructuredMesh->GetNumberOfCells();
+            ids->Resize(cellNum);
+            for (IGsize i = 0; i < cellNum; ++i) {
+                ids->SetValue(i, m_ProcessId);
+            }
+            input->GetAttributeSet()->AddScalar(IG_CELL, ids);
+        }
+    }
+
+    SetOutput(input);
+    return true;
+}
+
+IGAME_NAMESPACE_END
