32ce07aa6f80f00713952100d779dcecf764463d
Tutis(图特斯） <152156470+BuzzyKey@users.noreply.github.com>
2026-08-21 22:09:27 +0800

    Add GenerateProcessIdsFilter class definition
    GenerateProcessIdsFilter的头文件

---
+++ iGameCore/Filters/ProcessGet/iGameGenerateProcessIdsFilter.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef EOIGAME_IGAMECORE_PROCESSGET_IGAMEGENERATEPROCESSIDSFILTER_H
+#define EOIGAME_IGAMECORE_PROCESSGET_IGAMEGENERATEPROCESSIDSFILTER_H
+
+#include <iGameDataObject.h>
+#include <iGameFilter.h>
+#include <iGameModel.h>
+#include <iGamePoints.h>
+#include <iGameUnstructuredMesh.h>
+
+IGAME_NAMESPACE_BEGIN
+class GenerateProcessIdsFilter : public Filter {
+    I_OBJECT(GenerateProcessIdsFilter)
+
+public:
+    static Pointer New() { return new GenerateProcessIdsFilter; }
+
+    bool Execute() override;
+
+    void SetGeneratePointData(bool b) { m_GeneratePointData = b; }
+    bool GetGeneratePointData() const { return m_GeneratePointData; }
+
+    void SetGenerateCellData(bool b) { m_GenerateCellData = b; }
+    bool GetGenerateCellData() const { return m_GenerateCellData; }
+
+    void SetProcessId(int pid) { m_ProcessId = pid; }
+    int GetProcessId() const { return m_ProcessId; }
+
+protected:
+    GenerateProcessIdsFilter();
+    ~GenerateProcessIdsFilter() override = default;
+
+    int m_ProcessId{0};
+    bool m_GeneratePointData{true};
+    bool m_GenerateCellData{false};
+};
+IGAME_NAMESPACE_END
+#endif
