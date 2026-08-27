3b292551d9e7cdf45607a858be6d7c7840a1f084
Boluo Zhang <Zhang.Boluo@qq.com>
2026-08-28 00:17:41 +0800

    fix(gui):filter功能入口移动

--- Qt/src/IQCore/igQtMainWindow.cpp
+++ Qt/src/IQCore/igQtMainWindow.cpp
@@ -1560,41 +1560,6 @@ void igQtMainWindow::initAllFilters() {
         rendererWidget->update();
     });
 
-    connect(mesh_processing->addAction("多块模型表面提取"), &QAction::triggered, this, [&](bool checked) {
-        if (!rendererWidget) {
-            showDarkFramelessMessage(QStringLiteral("Warning"),
-                                     QStringLiteral("渲染器组件未初始化。"));
-        }
-
-        auto scene = rendererWidget->GetScene();
-        if (!scene) { 
-            showDarkFramelessMessage(QStringLiteral("Warning"), QStringLiteral("场景未初始化。"));
-        }
-
-        auto currentModel = scene->GetCurrentModel();
-        if (!currentModel) { 
-            showDarkFramelessMessage(QStringLiteral("Warning"), QStringLiteral("未能获取当前选定模型。")); 
-        }
-
-        auto obj = currentModel->GetDataObject();
-        if (!obj) {
-            showDarkFramelessMessage(QStringLiteral("Warning"), QStringLiteral("未能获取DataObject。"));
-        }
-
-        auto multiBlockFilter = MultiBlockGeometryFilter::New();
-        multiBlockFilter->SetInput(obj);
-        if (!multiBlockFilter->Execute()) {
-			showDarkFramelessMessage(QStringLiteral("Warning"), QStringLiteral("多块模型表面提取失败。"));
-			return;
-		}
-
-        auto multiBlockObj = multiBlockFilter->GetOutput();
-        multiBlockObj->SetName(obj->GetName() + "_MultiBlockSurface");
-        modelTreeWidget->addDataObjectToModelTree(multiBlockObj, Algorithm);
-
-        rendererWidget->update();
-
-    });
 
     //connect(mesh_processing->addAction("Test"), &QAction::triggered, this, [&](bool checked) {
     //    auto obj = rendererWidget->GetScene()->GetCurrentModel()->GetDataObject();
@@ -1979,6 +1944,42 @@ void igQtMainWindow::initAllFilters() {
             res->SetName(data->GetName());
             modelTreeWidget->addDataObjectToModelTree(res, Algorithm);
         }
+    });
+
+    connect(ui->menu_filters->addAction("多块模型表面提取"), &QAction::triggered, this, [&](bool checked) {
+        if (!rendererWidget) {
+            showDarkFramelessMessage(QStringLiteral("Warning"),
+                                     QStringLiteral("渲染器组件未初始化。"));
+        }
+
+        auto scene = rendererWidget->GetScene();
+        if (!scene) { 
+            showDarkFramelessMessage(QStringLiteral("Warning"), QStringLiteral("场景未初始化。"));
+        }
+
+        auto currentModel = scene->GetCurrentModel();
+        if (!currentModel) { 
+            showDarkFramelessMessage(QStringLiteral("Warning"), QStringLiteral("未能获取当前选定模型。")); 
+        }
+
+        auto obj = currentModel->GetDataObject();
+        if (!obj) {
+            showDarkFramelessMessage(QStringLiteral("Warning"), QStringLiteral("未能获取DataObject。"));
+        }
+
+        auto multiBlockFilter = MultiBlockGeometryFilter::New();
+        multiBlockFilter->SetInput(obj);
+        if (!multiBlockFilter->Execute()) {
+			showDarkFramelessMessage(QStringLiteral("Warning"), QStringLiteral("多块模型表面提取失败。"));
+			return;
+		}
+
+        auto multiBlockObj = multiBlockFilter->GetOutput();
+        multiBlockObj->SetName(obj->GetName() + "_MultiBlockSurface");
+        modelTreeWidget->addDataObjectToModelTree(multiBlockObj, Algorithm);
+
+        rendererWidget->update();
+
     });
 }
 
